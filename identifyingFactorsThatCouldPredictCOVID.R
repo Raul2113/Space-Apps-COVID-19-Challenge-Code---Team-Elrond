a528c96bc8c232465310dafb0aed6293f29fad6e
Raul2113 <57413003+Raul2113@users.noreply.github.com>
2020-05-31 22:54:20 -0500

    Update identifyingFactorsThatCouldPredictCOVID.R

--- identifyingFactorsThatCouldPredictCOVID.R
+++ identifyingFactorsThatCouldPredictCOVID.R
@@ -2,12 +2,18 @@ library(leaps)
 library(ggplot2)
 library(dplyr)
 library(broom)
+library(scatterplot3d)
 
 #Read in the dataset and remove any observations (countries and territories) that
 #are missing data with na.omit
 covid <- read.csv(file.choose(), na.strings = c("", "NA"))
 covid <- na.omit(covid)
 attach(covid)
+
+#Removing the two outliers of US and China to observe if the best model changes
+covid2 <- covid%>%
+  filter(GDP < 10^13)
+attach(covid2)
 
 sd(covCases)
 sd(covDeaths)
@@ -74,6 +80,31 @@ glance(lm(covDeaths ~ GDP))
 #(predicted # of COVID-19 cases) = 2700 + (6.64 x 10^-8)(GDP)
 #(predicted # of COVID-19 deaths) = 226 + (4.09 x 10^-9)(GDP)
 
+#Best model found for COVID-19 cases with Mallows' Cp when US and China
+#are removed, run with covid2 attached
+tidy(lm(covCases ~ GDP + waterlocked))
+glance(lm(covCases ~ GDP + waterlocked))
+
+#Best model found for COVID-19 deaths with Mallows' Cp when US and China
+#are removed, run with covid2 attached
+tidy(lm(covDeaths ~ GDP))
+glance(lm(covDeaths ~ GDP))
+
+#Best model found for COVID-19 cases with backwards elimination when US and
+#China are removed, run with covid2 attached
+tidy(lm(covCases ~ GDP + waterlocked))
+glance(lm(covCases ~ GDP + waterlocked))
+
+#Best model found for COVID-19 deaths with backwards elimination when US and
+#China are removed, run with covid2 attached
+tidy(lm(covDeaths ~ GDP))
+glance(lm(covDeaths ~ GDP))
+
+#Both of these methods agree for both models:
+#(predicted # of COVID-19 cases) = 11600 + (5.54 x 10^-8)(GDP)
+# - 21800(waterlocked)
+#(predicted # of COVID-19 deaths) = 78.2 + (5.08 x 10^-9)(GDP)
+
 #Two scatterplots with their best fit lines found by the four different methods
 #for visualization purposes
 covid%>%
@@ -84,6 +115,22 @@ covid%>%
        x = "Country/Territory GDP", y = "Number of COVID-19 Cases")
 
 covid%>%
+  ggplot(aes(x = GDP, y = covDeaths)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Best Fit Line with Mallows' Cp for Predicting COVID-19 Deaths",
+       x = "Country/Territory GDP", y = "Number of COVID-19 Deaths")
+
+#Creating a 3D scatterplot to visualize the regression plane for the best model
+#for predicting COVID-19 cases when the US and China are removed, run with 
+#covid2 attached
+s3d <- scatterplot3d(x = GDP, y = waterlocked, z = covCases, type = "h",
+                     color = "blue", angle = 55, pch = 16)
+s3d$plane3d(lm(covCases ~ GDP + waterlocked))
+
+#Scatterplot to visualize the best model for predicting COVID-19 deaths when
+#the US and China are removed, run with covid2 attached
+covid2%>%
   ggplot(aes(x = GDP, y = covDeaths)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
